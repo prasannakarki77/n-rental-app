3f6639b7495ce5d6e688cd48d3c8b1faba082652
prasannakarki77 <prasanna.karki77@gmail.com>
2022-05-12 16:56:26 +0545

    created curved bottom nav bar

--- lib/screens/dashboard_screen.dart
+++ lib/screens/dashboard_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import "package:curved_navigation_bar/curved_navigation_bar.dart";
 
 import '../components/customShape.dart';
 
@@ -10,22 +11,83 @@ class DashboardScreem extends StatefulWidget {
 }
 
 class _DashboardScreemState extends State<DashboardScreem> {
+  int bottomNavIndex = 2;
+
+  Color bgColorCar = const Color.fromRGBO(243, 243, 243, 1);
+  Color bgColorBike = const Color.fromRGBO(243, 243, 243, 1);
+  Color bgColorBus = const Color.fromRGBO(243, 243, 243, 1);
+  Color bgColorVan = const Color.fromRGBO(243, 243, 243, 1);
   String carIcon = "car-red.png";
   String bikeIcon = "bike-red.png";
   String busIcon = "bus-red.png";
   String vanIcon = "van-red.png";
-  Color bgcolor = const Color.fromRGBO(243, 243, 243, 1);
-  _iconClicked() {
-    setState(() {
+
+  _iconClicked(String vehicle) {
+    if (vehicle == "car") {
       carIcon = "car-white.png";
-      bgcolor = Colors.red;
-    });
+      bikeIcon = "bike-red.png";
+      busIcon = "bus-red.png";
+      vanIcon = "van-red.png";
+      bgColorCar = Colors.red;
+      bgColorBike =
+          bgColorVan = bgColorBus = const Color.fromRGBO(243, 243, 243, 1);
+    }
+    if (vehicle == "bike") {
+      carIcon = "car-red.png";
+      bikeIcon = "bike-white.png";
+      busIcon = "bus-red.png";
+      vanIcon = "van-red.png";
+      bgColorBike = Colors.red;
+      bgColorCar =
+          bgColorVan = bgColorBus = const Color.fromRGBO(243, 243, 243, 1);
+    }
+    if (vehicle == "van") {
+      carIcon = "car-red.png";
+      bikeIcon = "bike-red.png";
+      busIcon = "bus-red.png";
+      vanIcon = "van-white.png";
+      bgColorVan = Colors.red;
+      bgColorCar =
+          bgColorBike = bgColorBus = const Color.fromRGBO(243, 243, 243, 1);
+    }
+    if (vehicle == "bus") {
+      carIcon = "car-red.png";
+      bikeIcon = "bike-red.png";
+      busIcon = "bus-white.png";
+      vanIcon = "van-red.png";
+      bgColorBus = Colors.red;
+      bgColorCar =
+          bgColorVan = bgColorVan = const Color.fromRGBO(243, 243, 243, 1);
+    }
   }
 
   @override
   Widget build(BuildContext context) {
+    final items = <Widget>[
+      const Icon(
+        Icons.person,
+        size: 30,
+      ),
+      const Icon(
+        Icons.search,
+        size: 30,
+      ),
+      const Icon(
+        Icons.home,
+        size: 30,
+      ),
+      const Icon(
+        Icons.bookmark_added_rounded,
+        size: 30,
+      ),
+      const Icon(
+        Icons.settings,
+        size: 30,
+      ),
+    ];
     return SafeArea(
       child: Scaffold(
+        extendBody: true,
         appBar: AppBar(
           automaticallyImplyLeading: false,
           toolbarHeight: 130,
@@ -62,12 +124,14 @@ class _DashboardScreemState extends State<DashboardScreem> {
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: [
                 InkWell(
-                  onTap: () => _iconClicked(),
+                  onTap: () => setState(() {
+                    _iconClicked("car");
+                  }),
                   child: AnimatedContainer(
                     height: 70,
                     width: 80,
                     decoration: BoxDecoration(
-                        color: bgcolor,
+                        color: bgColorCar,
                         borderRadius: BorderRadius.circular(9),
                         boxShadow: const [
                           BoxShadow(
@@ -85,12 +149,14 @@ class _DashboardScreemState extends State<DashboardScreem> {
                   ),
                 ),
                 InkWell(
-                  onTap: () => _iconClicked(),
+                  onTap: () => setState(() {
+                    _iconClicked("bike");
+                  }),
                   child: AnimatedContainer(
                     height: 70,
                     width: 80,
                     decoration: BoxDecoration(
-                        color: bgcolor,
+                        color: bgColorBike,
                         borderRadius: BorderRadius.circular(9),
                         boxShadow: const [
                           BoxShadow(
@@ -108,12 +174,14 @@ class _DashboardScreemState extends State<DashboardScreem> {
                   ),
                 ),
                 InkWell(
-                  onTap: () => _iconClicked(),
+                  onTap: () => setState(() {
+                    _iconClicked("van");
+                  }),
                   child: AnimatedContainer(
                     height: 70,
                     width: 80,
                     decoration: BoxDecoration(
-                        color: bgcolor,
+                        color: bgColorVan,
                         borderRadius: BorderRadius.circular(9),
                         boxShadow: const [
                           BoxShadow(
@@ -131,12 +199,14 @@ class _DashboardScreemState extends State<DashboardScreem> {
                   ),
                 ),
                 InkWell(
-                  onTap: () => _iconClicked(),
+                  onTap: () => setState(() {
+                    _iconClicked("bus");
+                  }),
                   child: AnimatedContainer(
                     height: 70,
                     width: 80,
                     decoration: BoxDecoration(
-                        color: bgcolor,
+                        color: bgColorBus,
                         borderRadius: BorderRadius.circular(9),
                         boxShadow: const [
                           BoxShadow(
@@ -156,6 +226,22 @@ class _DashboardScreemState extends State<DashboardScreem> {
               ],
             )
           ],
+        ),
+        bottomNavigationBar: Theme(
+          data: Theme.of(context)
+              .copyWith(iconTheme: const IconThemeData(color: Colors.white)),
+          child: CurvedNavigationBar(
+            index: bottomNavIndex,
+            backgroundColor: Colors.transparent,
+            buttonBackgroundColor: Colors.red,
+            color: const Color.fromARGB(255, 0, 0, 0),
+            items: items,
+            animationCurve: Curves.easeInOut,
+            animationDuration: const Duration(milliseconds: 500),
+            onTap: (index) {
+              //Handle button tap
+            },
+          ),
         ),
       ),
     );
