a8222c447e0b5fca8c06ab34be679c5134a08239
prasannakarki77 <prasanna.karki77@gmail.com>
2022-07-15 22:04:51 +0545

    created booking update modal form and alert dialog

--- lib/screen/booking_detail_screen.dart
+++ lib/screen/booking_detail_screen.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:nrental/model/booking_vehicle.dart';
+import 'package:nrental/utils/show_message.dart';
 import 'package:nrental/utils/url.dart';
+
+import '../model/booking.dart';
 
 class BookingDetailScreen extends StatefulWidget {
   const BookingDetailScreen({Key? key}) : super(key: key);
@@ -10,6 +13,50 @@ class BookingDetailScreen extends StatefulWidget {
 }
 
 class _BookingDetailScreenState extends State<BookingDetailScreen> {
+  final _daysController = TextEditingController();
+  final _phoneController = TextEditingController();
+  final _addressController = TextEditingController();
+
+  DateTime pickedDate = DateTime.now();
+  TimeOfDay time = TimeOfDay.now();
+
+  final _timeController = TextEditingController();
+  final _dateController = TextEditingController();
+
+  _dateSelected() {
+    _dateController.text =
+        "${pickedDate.year} - ${pickedDate.month} - ${pickedDate.day}";
+  }
+
+  _timeSelected() {
+    _timeController.text = "${time.hour}: ${time.minute}";
+  }
+
+  _displayMessage(bool isBooked) {
+    if (isBooked) {
+      displaySuccessMessage(context, "Booking success");
+    } else {
+      displayErrorMessage(context, "Booking Failed");
+    }
+  }
+
+  _setTextFieldValues(booking) {
+    _daysController.text = booking.no_of_days.toString() ?? "";
+    _dateController.text = booking.booking_date ?? "";
+    _addressController.text = booking.address ?? "";
+    _phoneController.text = booking.contact_no ?? "";
+    _timeController.text = booking.booking_time ?? "";
+  }
+
+//  _updateBooking(Booking booking) async {
+//     bool isUpdated = await UserRepository().updateProfile(user);
+//     if (isUpdated) {
+//       _displayMessage(true);
+//     } else {
+//       _displayMessage(false);
+//     }
+//   }
+
   @override
   Widget build(BuildContext context) {
     BookingVehicle booking =
@@ -76,7 +123,8 @@ class _BookingDetailScreenState extends State<BookingDetailScreen> {
                             ),
                           ),
                           onPressed: () {
-                            // _bookingForm(context);
+                            _setTextFieldValues(booking);
+                            _bookingForm(context);
                           },
                         ),
                       ),
@@ -107,7 +155,7 @@ class _BookingDetailScreenState extends State<BookingDetailScreen> {
                             ),
                           ),
                           onPressed: () {
-                            // _bookingForm(context);
+                            _showMyDialog();
                           },
                         ),
                       ),
@@ -342,5 +390,284 @@ class _BookingDetailScreenState extends State<BookingDetailScreen> {
         ),
       ),
     );
+  }
+
+  Widget _dateField() => TextFormField(
+        readOnly: true,
+        controller: _dateController,
+        style: const TextStyle(
+          fontSize: 18,
+          fontWeight: FontWeight.w500,
+        ),
+        decoration: InputDecoration(
+          fillColor: Colors.white,
+          filled: true,
+          labelText: "Booking Date",
+          suffixIcon: IconButton(
+            icon: const Icon(Icons.calendar_month),
+            onPressed: () {
+              _pickDate();
+            },
+          ),
+          focusedBorder: OutlineInputBorder(
+              borderSide: const BorderSide(
+                color: Color.fromARGB(255, 234, 98, 7),
+                width: 2.0,
+              ),
+              borderRadius: BorderRadius.circular(10)),
+          enabledBorder: OutlineInputBorder(
+            borderSide: const BorderSide(
+              color: Color.fromARGB(255, 135, 142, 135),
+              width: 2.0,
+            ),
+            borderRadius: BorderRadius.circular(10),
+          ),
+        ),
+      );
+  Widget _timeField() => TextFormField(
+        readOnly: true,
+        controller: _timeController,
+        style: const TextStyle(
+          fontSize: 18,
+          fontWeight: FontWeight.w500,
+        ),
+        decoration: InputDecoration(
+          fillColor: Colors.white,
+          filled: true,
+          labelText: "Booking Time",
+          suffixIcon: IconButton(
+            icon: const Icon(Icons.timer),
+            onPressed: () {
+              _pickTime();
+            },
+          ),
+          focusedBorder: OutlineInputBorder(
+              borderSide: const BorderSide(
+                color: Color.fromARGB(255, 234, 98, 7),
+                width: 2.0,
+              ),
+              borderRadius: BorderRadius.circular(10)),
+          enabledBorder: OutlineInputBorder(
+            borderSide: const BorderSide(
+              color: Color.fromARGB(255, 135, 142, 135),
+              width: 2.0,
+            ),
+            borderRadius: BorderRadius.circular(10),
+          ),
+        ),
+      );
+  Widget _daysField() => TextFormField(
+        keyboardType: TextInputType.number,
+        controller: _daysController,
+        style: const TextStyle(
+          fontSize: 18,
+          fontWeight: FontWeight.w500,
+        ),
+        decoration: InputDecoration(
+          fillColor: Colors.white,
+          filled: true,
+          labelText: "No of Days",
+          suffixIcon: const Icon(Icons.sunny),
+          focusedBorder: OutlineInputBorder(
+              borderSide: const BorderSide(
+                color: Color.fromARGB(255, 234, 98, 7),
+                width: 2.0,
+              ),
+              borderRadius: BorderRadius.circular(10)),
+          enabledBorder: OutlineInputBorder(
+            borderSide: const BorderSide(
+              color: Color.fromARGB(255, 135, 142, 135),
+              width: 2.0,
+            ),
+            borderRadius: BorderRadius.circular(10),
+          ),
+        ),
+      );
+  Widget _phoneField() => TextFormField(
+        keyboardType: TextInputType.number,
+        controller: _phoneController,
+        style: const TextStyle(
+          fontSize: 18,
+          fontWeight: FontWeight.w500,
+        ),
+        decoration: InputDecoration(
+          fillColor: Colors.white,
+          filled: true,
+          labelText: "Phone No",
+          suffixIcon: const Icon(Icons.call),
+          focusedBorder: OutlineInputBorder(
+              borderSide: const BorderSide(
+                color: Color.fromARGB(255, 234, 98, 7),
+                width: 2.0,
+              ),
+              borderRadius: BorderRadius.circular(10)),
+          enabledBorder: OutlineInputBorder(
+            borderSide: const BorderSide(
+              color: Color.fromARGB(255, 135, 142, 135),
+              width: 2.0,
+            ),
+            borderRadius: BorderRadius.circular(10),
+          ),
+        ),
+      );
+
+  Widget _addressField() => TextFormField(
+        controller: _addressController,
+        style: const TextStyle(
+          fontSize: 18,
+          fontWeight: FontWeight.w500,
+        ),
+        decoration: InputDecoration(
+          fillColor: Colors.white,
+          filled: true,
+          labelText: "Address",
+          suffixIcon: const Icon(Icons.location_on),
+          focusedBorder: OutlineInputBorder(
+              borderSide: const BorderSide(
+                color: Color.fromARGB(255, 234, 98, 7),
+                width: 2.0,
+              ),
+              borderRadius: BorderRadius.circular(10)),
+          enabledBorder: OutlineInputBorder(
+            borderSide: const BorderSide(
+              color: Color.fromARGB(255, 135, 142, 135),
+              width: 2.0,
+            ),
+            borderRadius: BorderRadius.circular(10),
+          ),
+        ),
+      );
+
+  _pickDate() async {
+    DateTime? date = await showDatePicker(
+      context: context,
+      firstDate: DateTime(DateTime.now().year - 5),
+      lastDate: DateTime(DateTime.now().year + 5),
+      initialDate: pickedDate,
+    );
+    if (date != null) {
+      setState(() {
+        pickedDate = date;
+        _dateSelected();
+      });
+    }
+  }
+
+  _pickTime() async {
+    TimeOfDay? t = await showTimePicker(
+      context: context,
+      initialTime: time,
+    );
+    if (t != null) {
+      setState(() {
+        time = t;
+        _timeSelected();
+      });
+    }
+  }
+
+  void _bookingForm(context) => showModalBottomSheet<void>(
+        backgroundColor: Colors.transparent,
+        context: context,
+        isScrollControlled: true,
+        builder: (BuildContext context) {
+          return Container(
+            height: MediaQuery.of(context).size.height * 0.65,
+            decoration: const BoxDecoration(
+              // color: Color.fromARGB(255, 242, 238, 238),
+              color: Colors.white,
+              borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(20.0),
+                  topRight: Radius.circular(20.0)),
+            ),
+            child: Center(
+              child: Padding(
+                padding: const EdgeInsets.all(20),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    _dateField(),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    _timeField(),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    _daysField(),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    _phoneField(),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    _addressField(),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    SizedBox(
+                      height: 50,
+                      width: double.infinity,
+                      child: ElevatedButton.icon(
+                        label: const Text(
+                          'Update Booking',
+                          style: TextStyle(
+                            fontSize: 18,
+                            fontWeight: FontWeight.w800,
+                            color: Colors.white,
+                          ),
+                        ),
+                        icon: const Icon(Icons.edit_note_rounded),
+                        style: ElevatedButton.styleFrom(
+                          primary: Colors.green,
+                          shape: RoundedRectangleBorder(
+                            borderRadius:
+                                BorderRadius.circular(10), // <-- Radius
+                          ),
+                        ),
+                        onPressed: () {
+                          Booking booking = Booking(
+                            no_of_days: _daysController.text,
+                            booking_date: _dateController.text,
+                            booking_time: _timeController.text,
+                            address: _addressController.text,
+                            contact_no: _phoneController.text,
+                          );
+
+                          // _bookVehicle(
+                          //     booking, vehicle.id, vehicle.vehicle_name);
+
+                          Navigator.pop(context);
+                        },
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          );
+        },
+      );
+
+  Future<Future<String?>> _showMyDialog() async {
+    return showDialog<String>(
+        context: context,
+        builder: (BuildContext context) => AlertDialog(
+              title: const Text('Cancel Booking'),
+              content:
+                  const Text('Are you sure you want to cancel this booking?'),
+              actions: <Widget>[
+                TextButton(
+                  onPressed: () => Navigator.pop(context, 'OK'),
+                  child: const Text('Yes'),
+                ),
+                TextButton(
+                  onPressed: () => Navigator.pop(context, 'No'),
+                  child: const Text('No'),
+                ),
+              ],
+            ));
   }
 }
