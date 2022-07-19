8fbd59bbd113c3db6b64e0831185840dbe19a3a8
prasannakarki77 <prasanna.karki77@gmail.com>
2022-07-19 16:17:29 +0545

    added search and advanced filter functionality

--- lib/screen/search_screen.dart
+++ lib/screen/search_screen.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:nrental/components/category_widget.dart';
 import 'package:nrental/model/vehicle.dart';
 import 'package:nrental/repository/vehicle_repository.dart';
 import 'package:nrental/response/vehicle_response.dart';
@@ -15,8 +14,8 @@ class SearchScreen extends StatefulWidget {
 }
 
 class _SearchScreenState extends State<SearchScreen> {
-  String query = '';
-
+  String category = '';
+  String searchQuery = '';
   Color bgColorCar = const Color.fromRGBO(243, 243, 243, 1);
   Color bgColorBike = const Color.fromRGBO(243, 243, 243, 1);
   Color bgColorBus = const Color.fromRGBO(243, 243, 243, 1);
@@ -25,9 +24,10 @@ class _SearchScreenState extends State<SearchScreen> {
   String bikeIcon = "bike-red.png";
   String busIcon = "bus-red.png";
   String vanIcon = "van-red.png";
+  final _searchQueryController = TextEditingController();
 
   _iconClicked(String vehicle) {
-    if (vehicle == "car") {
+    if (vehicle == "Car") {
       carIcon = "car-white.png";
       bikeIcon = "bike-red.png";
       busIcon = "bus-red.png";
@@ -36,7 +36,7 @@ class _SearchScreenState extends State<SearchScreen> {
       bgColorBike =
           bgColorVan = bgColorBus = const Color.fromRGBO(243, 243, 243, 1);
     }
-    if (vehicle == "bike") {
+    if (vehicle == "Bike") {
       carIcon = "car-red.png";
       bikeIcon = "bike-white.png";
       busIcon = "bus-red.png";
@@ -45,7 +45,7 @@ class _SearchScreenState extends State<SearchScreen> {
       bgColorCar =
           bgColorVan = bgColorBus = const Color.fromRGBO(243, 243, 243, 1);
     }
-    if (vehicle == "van") {
+    if (vehicle == "Van") {
       carIcon = "car-red.png";
       bikeIcon = "bike-red.png";
       busIcon = "bus-red.png";
@@ -54,7 +54,7 @@ class _SearchScreenState extends State<SearchScreen> {
       bgColorCar =
           bgColorBike = bgColorBus = const Color.fromRGBO(243, 243, 243, 1);
     }
-    if (vehicle == "bus") {
+    if (vehicle == "Bus") {
       carIcon = "car-red.png";
       bikeIcon = "bike-red.png";
       busIcon = "bus-white.png";
@@ -67,20 +67,123 @@ class _SearchScreenState extends State<SearchScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        // searchBox(),
-        const CategoryWidget(),
-        const SizedBox(
-          height: 20,
-        ),
-        Expanded(
-          child: FutureBuilder<VehicleResponse?>(
+    return SingleChildScrollView(
+      child: Column(
+        children: [
+          // searchBox(),
+          // const CategoryWidget(),
+          Padding(
+            padding: const EdgeInsets.all(10),
+            child: Container(
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(20),
+                boxShadow: const [
+                  BoxShadow(
+                    color: Color.fromRGBO(0, 0, 0, 0.15),
+                    spreadRadius: 0,
+                    blurRadius: 5,
+                    offset: Offset(0, 4),
+                  ),
+                ],
+              ),
+              child: Padding(
+                padding: const EdgeInsets.fromLTRB(20, 5, 20, 5),
+                child: TextFormField(
+                  controller: _searchQueryController,
+                  decoration: InputDecoration(
+                    fillColor: Colors.black,
+                    border: InputBorder.none,
+                    hintText: "Search vehicles",
+                    hintStyle: const TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 18,
+                        color: Colors.grey),
+                    suffixIcon: Align(
+                      widthFactor: 1.0,
+                      heightFactor: 1.0,
+                      child: IconButton(
+                        icon: const Icon(
+                          Icons.search,
+                          size: 30,
+                        ),
+                        onPressed: () {
+                          setState(() {
+                            searchQuery = _searchQueryController.text;
+                            print(searchQuery);
+                          });
+                        },
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ),
+
+          const SizedBox(
+            height: 20,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              vehicleCategory(bgColorCar, carIcon, "Car"),
+              vehicleCategory(bgColorBike, bikeIcon, "Bike"),
+              vehicleCategory(bgColorVan, vanIcon, "Van"),
+              vehicleCategory(bgColorBus, busIcon, "Bus")
+            ],
+          ),
+          const SizedBox(
+            height: 5,
+          ),
+          Align(
+            alignment: Alignment.centerRight,
+            child: SizedBox(
+              width: 100,
+              child: TextButton(
+                style: TextButton.styleFrom(
+                  textStyle: const TextStyle(fontSize: 14),
+                ),
+                onPressed: () {
+                  setState(() {
+                    category = "";
+                    searchQuery = "";
+                  });
+                },
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: const [
+                    Text(
+                      'View All',
+                      style: TextStyle(fontSize: 14),
+                    ),
+                    Icon(Icons.arrow_right),
+                  ],
+                ),
+              ),
+            ),
+          ),
+          FutureBuilder<VehicleResponse?>(
             future: VehicleRepository().getVehicles(),
             builder: (context, snapshot) {
               if (snapshot.connectionState == ConnectionState.done) {
                 if (snapshot.hasData) {
-                  List<Vehicle> lstVehicles = snapshot.data!.data!;
+                  List<Vehicle> lstVehiclesAll = snapshot.data!.data!;
+                  List<Vehicle> lstVehicles;
+                  if (searchQuery != "") {
+                    List<Vehicle> lstVehiclesFiltered = lstVehiclesAll
+                        .where((v) =>
+                            v.vehicle_name!.toLowerCase().contains(searchQuery))
+                        .toList();
+                    lstVehicles = lstVehiclesFiltered;
+                  } else if (category != "") {
+                    List<Vehicle> lstVehiclesFiltered = lstVehiclesAll
+                        .where((v) => v.vehicle_category == category)
+                        .toList();
+                    lstVehicles = lstVehiclesFiltered;
+                  } else {
+                    lstVehicles = lstVehiclesAll;
+                  }
                   return GridView.builder(
                     shrinkWrap: true,
                     physics: const NeverScrollableScrollPhysics(),
@@ -116,15 +219,18 @@ class _SearchScreenState extends State<SearchScreen> {
               }
             },
           ),
-        ),
-      ],
+        ],
+      ),
     );
   }
 
   Widget vehicleCategory(bgColor, icon, vehicleType) {
     return InkWell(
       onTap: () => setState(() {
-        _iconClicked("$vehicleType");
+        category = vehicleType;
+        searchQuery = "";
+        // _iconClicked(vehicleType);
+        print(category);
       }),
       child: AnimatedContainer(
         height: 70,
@@ -253,7 +359,7 @@ class _SearchScreenState extends State<SearchScreen> {
   }
 
   Widget searchBox() => SearchWidget(
-        text: query,
+        text: category,
         hintText: 'Search  Vehicles',
         onChanged: searchVehicle,
       );
